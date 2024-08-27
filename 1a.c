4c000f0beb0aaee7c72af420c80f7b25e744ba3d
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-27 17:07:28 +0530

    First Question

--- 1a.c
+++ 1a.c
@@ -1,7 +1,7 @@
 /*Name: Sakina Baranwala
-Reg. No: MT2024130
-Problem Statement: Create the following types of a files using (i) shell command (ii) system call
-                  a. soft link (symlink system call/* 
+*Reg. No: MT2024130
+*Problem Statement: Create the following types of a files using (i) shell command (ii) system call
+                  a. soft link (symlink system call*/ 
 
 
 
@@ -10,7 +10,7 @@ Problem Statement: Create the following types of a files using (i) shell command
 
 int main() {
 
-	if (symlink("/home/sakina/Desktop/SSLabs/SoftLinkTarget.txt","/home/sakina/Desktop/SSLabs/NewSoftLink.txt") == -1){
+	if (symlink("/home/sakina/my-repo/SoftLinkTarget.txt","/home/sakina/my-repo/NewSoftLink.txt") == -1){
 		perror("syslink error");
 		return 1;
 	}
@@ -18,3 +18,8 @@ int main() {
 	return 0;
 }
 
+
+/*Output:
+ *
+ * lrwxrwxrwx 1 sakina sakina    39 Aug 27 17:04 NewSoftLink.txt -> /home/sakina/my-repo/SoftLinkTarget.txt
+*/
