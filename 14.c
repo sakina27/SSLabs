cf3f8da3dbe37ee422a4934a10da83b861346499
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-27 17:08:19 +0530

    14th Question

---
+++ 14.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+    struct stat fileInfo;
+
+    if (stat(argv[1], &fileInfo) < 0) {
+        perror("Error getting file information");
+        return 1;
+    }
+
+    if (S_ISREG(fileInfo.st_mode)) {
+        printf("%s is a regular file.\n", argv[1]);
+    } else if (S_ISDIR(fileInfo.st_mode)) {
+        printf("%s is a directory.\n", argv[1]);
+    } else if (S_ISCHR(fileInfo.st_mode)) {
+        printf("%s is a character device.\n", argv[1]);
+    } else if (S_ISBLK(fileInfo.st_mode)) {
+        printf("%s is a block device.\n", argv[1]);
+    } else if (S_ISFIFO(fileInfo.st_mode)) {
+        printf("%s is a FIFO (named pipe).\n", argv[1]);
+    } else if (S_ISLNK(fileInfo.st_mode)) {
+        printf("%s is a symbolic link.\n", argv[1]);
+    } else if (S_ISSOCK(fileInfo.st_mode)) {
+        printf("%s is a socket.\n", argv[1]);
+    }
+
+   return 0;
+
+}
+
+
+/*Output:
+ *
+ *
+ * sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ ./14 /home/sakina/my-repo
+/home/sakina/my-repo is a directory.
+sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ ./14 readFile.txt
+readFile.txt is a regular file.
+sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ ./14 FifoFile
+FifoFile is a FIFO (named pipe).
+*/
