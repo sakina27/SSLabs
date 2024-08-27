111506ea3b82449208654a85146980287ff916a6
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-27 17:19:51 +0530

    First Question

--- 1b.c
+++ 1b.c
@@ -12,10 +12,32 @@ Problem Statement: Create the following types of a files using (i) shell command
 
 int main() {
 
-	if (link("/home/sakina/Desktop/SSLabs/HardLinkTarget.txt","/home/sakina/Desktop/SSLabs/NewHardLink.txt") == -1){
+	if (link("/home/sakina/my-repo/HardLinkTarget.txt","/home/sakina/my-repo/NewHardLink.txt") == -1){
 		perror("syslink error");
 		return 1;
 	}
 
 	return 0;
 }
+
+
+
+/*Output:
+ *
+ *
+ * -rw-rw-r-- 2 sakina sakina     0 Aug 27 11:13 HardLinkTarget.txt
+-rw-rw-r-- 2 sakina sakina     0 Aug 27 11:13 NewHardLink.txt
+
+./1b
+sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ cat HardLinkTarget.txt
+Hi!
+This is Hard Link File.
+sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ cat NewHardLink.txt
+Hi!
+This is Hard Link File.
+sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ rm HardLinkTarget.txt
+sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ cat NewHardLink.txt
+Hi!
+This is Hard Link File*/.
+
+
