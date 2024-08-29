c6605ee4530125d732b69dc5c5f29abc8ebcfb96
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-30 00:23:36 +0530

    22nd Question

--- 22.c
+++ 22.c
@@ -1,7 +1,12 @@
-/*Name: Sakina Baranwala
-* Reg No-MT2024130
-* Problem Statement: 22. Write a program, open a file, call fork, and then write to the file by both the child as well as the
-parent processes. Check output of the file.*/
+/*
+============================================================================
+Name : 22.c
+Author : Sakina Baranwala
+Description :Write a program, open a file, call fork, and then write to the file by both the child as well as the
+parent processes. Check output of the file.
+Date: 29th Aug, 2024.
+============================================================================
+*/
 
 
 
