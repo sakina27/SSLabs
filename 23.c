de9aac62964b2d57aa92b13c127ec6fa2281bdb2
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-29 15:05:00 +0530

    23rd Question

---
+++ 23.c
@@ -0,0 +1,45 @@
+/*Name: Sakina Baranwala
+ * Reg No - MT2024130
+ * Problem Statement - 23. Write a program to create a Zombie state of the running program.*/
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    } else if (pid == 0) {
+        printf("Child process (PID: %d) starts exit...\n", getpid());
+        exit(0); 
+    } else {
+        printf("Parent process (PID: %d) is sleeping...\n", getpid());
+        sleep(10); //child becomes zombie
+
+        wait(NULL);
+        printf("zombie child process is cleaned up by parent process\n");
+    }
+
+    return 0;
+}
+
+
+
+/*Output:
+ *
+ * sakina@sakina-VivoBook-ASUSLaptop-X515EA-X515EA:~/my-repo$ ./23
+Parent process (PID: 9960) is sleeping...
+Child process (PID: 9961) starts exit...
+zombie child process is cleaned up by parent process
+*/
