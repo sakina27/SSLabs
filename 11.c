3a2fb6b63b8b08c9f4f91655434e6d1d82aa8a27
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-30 00:06:54 +0530

    11th Question

--- 11.c
+++ 11.c
@@ -1,10 +1,15 @@
-/*Name:Sakina Baranwala
- * reg No: MT2024130
- * Problem Statement: 11. Write a program to open a file, duplicate the file descriptor and append the file with both the
+/*
+============================================================================
+Name : 11.c
+Author : Sakina Baranwala
+Description :Write a program to open a file, duplicate the file descriptor and append the file with both the
 descriptors and check whether the file is updated properly or not.
 a. use dup
 b. use dup2
-c. use fcntl*/
+c. use fcntl
+Date: 27th Aug, 2024.
+============================================================================
+*/
 
 
 
