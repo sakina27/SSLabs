3d7f9899cc66ef92e5449b3aedc6e86732890753
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-29 23:57:48 +0530

    5th Question

--- 5.c
+++ 5.c
@@ -1,7 +1,12 @@
-/*Name: Sakina Baranwala
- * Reg No - MT2024130
- * Problem Statement - 5. Write a program to create five new files with infinite loop. Execute the program in the background
-and check the file descriptor table at /proc/pid/fd.*/
+/*
+============================================================================
+Name : 5.c
+Author : Sakina Baranwala
+Description : Write a program to create five new files with infinite loop. Execute the program in the background
+and check the file descriptor table at /proc/pid/fd.
+Date: 27th Aug, 2024.
+============================================================================
+*/
 
 
 
