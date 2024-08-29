68b050db234a2ba0584c2ce428a79d5f497aa30e
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-30 00:05:42 +0530

    10th Question

--- 10.c
+++ 10.c
@@ -1,9 +1,15 @@
-/* Name : Sakina Baranwala
- * Reg no : MT2024130
- * Problem Statement : 10. Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10
+/*
+============================================================================
+Name : 10.c
+Author : Sakina Baranwala
+Description :10. Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10
 bytes (use lseek) and write again 10 bytes.
 a. check the return value of lseek
-b. open the file with od and check the empty spaces in between the data.*/
+b. open the file with od and check the empty spaces in between the data.
+Date: 27th Aug, 2024.
+============================================================================
+*/
+
 
 #include <stdio.h>
 #include <unistd.h>
