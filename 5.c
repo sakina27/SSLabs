bda7e3730569bfa911d73a635de75320f1d7dac7
Sakina27 <sakinabaranwala123@gmail.com>
2024-08-29 23:15:24 +0530

    5th Question

--- 5.c
+++ 5.c
@@ -1,3 +1,12 @@
+/*Name: Sakina Baranwala
+ * Reg No - MT2024130
+ * Problem Statement - 5. Write a program to create five new files with infinite loop. Execute the program in the background
+and check the file descriptor table at /proc/pid/fd.*/
+
+
+
+
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
